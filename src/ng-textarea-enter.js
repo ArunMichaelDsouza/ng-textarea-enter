e10cc6414906b7d03683fdc49097d44b03edb431
ArunMichaelDsouza <amdsouza92@gmail.com>
2017-06-10 00:16:28 +0530

    Build updated

--- src/ng-textarea-enter.js
+++ src/ng-textarea-enter.js
@@ -1,59 +1,59 @@
 /*
-    ng-textarea-enter v0.1.6
+    ng-textarea-enter v0.2.1
     Copyright (c) 2016 Arun Michael Dsouza (amdsouza92@gmail.com)
     Licence: MIT
     Demo on CodePen - http://codepen.io/amdsouza92/pen/pyNMjQ
 */
 "use strict";
 angular.module('ngTextareaEnter', []).directive('ngTextareaEnter', function() {
-	return {
-		restrict: 'A',
-		link: function(scope, elem, attrs) {
+    return {
+        restrict: 'A',
+        link: function(scope, elem, attrs) {
 
-			// Detecting key down event
-			elem.bind('keydown', function(event) {
-				
-				var code = event.keyCode || event.which;
+            // Detecting key down event
+            elem.bind('keydown', function(event) {
 
-				// Detecting enter key press
-				if (code === 13) {
+                var code = event.keyCode || event.which;
 
-					// Checking element to be textarea
-					if(elem[0].type == 'textarea') {
+                // Detecting enter key press
+                if (code === 13) {
 
-						// used to get path for controllerAs syntax
-						function path(obj, path, def) {
-								var i, len;
+                    // Checking element to be textarea
+                    if (elem[0].type == 'textarea') {
 
-								for(i = 0,path = path.split('.'), len = path.length; i < len; i++) {
-										if(!obj || typeof obj !== 'object') return def;
-										obj = obj[path[i]];
-								}
+                        // used to get path for controllerAs syntax
+                        function path(obj, path, def) {
+                            var i, len;
 
-								if(obj === undefined) return def;
-								return obj;
-						}
+                            for (i = 0, path = path.split('.'), len = path.length; i < len; i++) {
+                                if (!obj || typeof obj !== 'object') return def;
+                                obj = obj[path[i]];
+                            }
 
-						// Determine scope model
-						var ngModel = path(scope, attrs.ngModel);
+                            if (obj === undefined) return def;
+                            return obj;
+                        }
 
-						// Checking scope model to be valid
-						if(ngModel !== undefined && ngModel !== '') {
+                        // Determine scope model
+                        var ngModel = path(scope, attrs.ngModel);
 
-							// Detecting shift/ctrl/alt key press
-							if (!event.shiftKey && !event.ctrlKey && !event.altKey) {
-								event.preventDefault();
-								scope.$apply(attrs.ngTextareaEnter);
-							}
+                        // Checking scope model to be valid
+                        if (ngModel !== undefined && ngModel !== '') {
 
-						}
+                            // Detecting shift/ctrl/alt key press
+                            if (!event.shiftKey && !event.ctrlKey && !event.altKey) {
+                                event.preventDefault();
+                                scope.$apply(attrs.ngTextareaEnter);
+                            }
 
-					}
+                        }
 
-				}
+                    }
 
-			});
+                }
 
-		}
-	}
+            });
+
+        }
+    }
 });
